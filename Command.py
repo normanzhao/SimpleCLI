b8999a1c2f866cb4c073dc6023f4057428d39e90
Norman Zhao <normanlz.zhao@gmail.com>
2018-07-12 21:35:48 -0400

    Initial commit

---
+++ Command.py
@@ -0,0 +1,42 @@
+known_commands = {}
+
+def default_run(options, arg):
+    "Command was not defined"
+    print "Undefined command"
+    pass
+
+class Command():
+  def verify_options(self,options):
+    for option in options:
+      if option not in self.options:
+        print option, "is not a valid option"
+        return False
+    return True
+
+  def __call__(self,options,args):
+    if self.verify_options(options):
+      self.run(options,args)
+
+  def __help__(self):
+    print self.description, self.usage
+
+  def __init__(self, name="",aliases="",options="", usage="", run=None):
+    known_commands[name] = name
+    if aliases != "":
+      for alias in aliases.split(","):
+        known_commands[alias] = name 
+    self.options = options
+    self.usage = usage
+    if run == None:
+      self.description=default_run.__doc__
+      self.run = default_run
+      self.usage = "[OPTION] -h"
+    else:
+      self.description = run.__doc__
+      if self.description == None:
+        self.description = "No description found for this command"
+      self.run = run
+
+
+invalid_command = Command(name="invalid_command",aliases="",options="", run=default_run)
+
---
+++ SimpleCLI.py
@@ -0,0 +1,53 @@
+import Command
+
+class SimpleCLI():
+  functions = None
+
+  def show_help(self):
+    "Prints all help for all defined functions"
+    known_commands = set(Command.known_commands.values())
+    known_commands.remove('invalid_command')
+    for command in known_commands:
+      print command,": ",
+      getattr(self.functions,command).__help__()
+
+
+  def parse(self):
+    """accept an incoming string, get the first term as a command, terms starting with - as flags, and the remaining is the arg"""
+    input_string = raw_input(">").split(" ")
+    com = input_string[0]
+    options = []
+    issue = []
+    for term in input_string[1:]:
+      if term.startswith('-'):
+        options += list(term[1:].strip())
+        continue
+      issue.append(term)
+    return com,options," ".join(issue)
+  
+  def program_loop(self):
+    """infinite loop to run the program"""
+    while True:
+      com, options, arg = self.parse()
+      if com =="":
+        continue
+      elif com == "help":
+        self.show_help()
+        continue
+      com = Command.known_commands[com]
+      try:
+        if "h" in options:
+          getattr(self.functions,com).__help__()
+          continue
+        getattr(self.functions,com)(options,arg)
+      except:
+        Command.invalid_command
+
+
+  def __init__(self, functions):
+    self.functions = functions
+    self.program_loop()
+
+
+  
+
